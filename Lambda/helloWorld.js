63a6bc77efcaea0103fad2860ec2d1f241d40785
sunnypawar <sunny.s.pawar@gmail.com>
2020-12-27 16:14:15 +0530

    14th Commit

--- Lambda/helloWorld.js
+++ Lambda/helloWorld.js
@@ -5,16 +5,16 @@
 const serverless = require('serverless-http');
 
 const express = require('express'); 
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.get('/api/info', (req, res) => {
+const helloworld = express();
+helloworld.use(express.urlencoded({ extended: true }));
+helloworld.use(express.json());
+helloworld.get('/api/info', (req, res) => {
   // res.send({ application: 'Sunny_sample-app', version: '1.0' });
   res.send({ statusCode: 200, body: "Hello Sunny-1"});
 });
-app.post('/api/v1/getback', (req, res) => {
+helloworld.post('/api/v1/getback', (req, res) => {
   res.send({ ...req.body });
 });
-app.listen(3000, () => console.log(`Listening on: 3000`));
+helloworld.listen(3000, () => console.log(`Listening on: 3000`));
 
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(helloworld);
